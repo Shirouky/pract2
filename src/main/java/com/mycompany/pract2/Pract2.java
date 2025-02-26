bc1c093d27d84572b62ef1c618467f295289f1f4
Shirouky <83469591+Shirouky@users.noreply.github.com>
2025-02-26 11:30:52 +0300

    Initial commit

---
+++ src/main/java/com/mycompany/pract2/Pract2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pract2;
+
+/**
+ *
+ * @author luba_
+ */
+public class Pract2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
